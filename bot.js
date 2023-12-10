a6bee11e58bbc670c11b323e7b40e8e8cd453b04
Myhailo Riznyk <myhailo@MBP-Myhailo.lan>
2023-12-10 15:05:34 +0200

    m

--- bot.js
+++ bot.js
@@ -2,24 +2,23 @@ const qrcode = require('qrcode-terminal')
 const { client } = require('./client')
 const { getAnswer } = require('./gpt')
 
-client.sendMessageToAll().then(() => {
-    client.on('qr', qr => {
-        qrcode.generate(qr, {small: true})
-    })
-    
-    client.on('ready', async () => {
-        await client.sendMessageToAll()
-        console.log('Client is ready!')
-    })
-    
-    client.on('message', async message => {
-        try {
-            const answer = await getAnswer(message.from, message.body)
-            await message.reply(answer)
-        } catch {
-            console.log('Can`t run new thread.ERROR')
-        }
-    })
-    
-    client.initialize()
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true})
 })
+
+client.on('ready', async () => {
+    await client.sendMessageToAll()
+    console.log('Client is ready!')
+})
+
+client.on('message', async message => {
+    try {
+        const answer = await getAnswer(message.from, message.body)
+        await message.reply(answer)
+    } catch {
+        console.log('Can`t run new thread.ERROR')
+    }
+})
+
+client.initialize()
